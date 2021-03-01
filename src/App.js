aff906f4b4663ac4d62837cb175fecbc90ef4f96
mahdi4k <falahati.mahdi@yahoo.com>
2021-03-01 23:00:22 +0330

    orderOpen completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './all.min.css';
 import React from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
